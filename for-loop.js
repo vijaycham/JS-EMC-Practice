ef0b30f987b045b1b83fae9f2e1cb01f5b0e5cd5
vijaycham <104141481+vijaycham@users.noreply.github.com>
2024-05-13 13:18:05 +0530

    Practice upto DOM

---
+++ for-loop.js
@@ -0,0 +1,26 @@
+// for(count=1;count<=6;count=count+1){
+//   console.log("Vijay")
+// }
+
+// for (num = 1; num <= 10; num = num + 1) {
+//   console.log(num);
+// }
+
+// for (num = 1; num <= 10; num = num + 2) {
+//   console.log(num);
+// }
+// for (num = 10; num >= 1; num = num-1) {
+//   console.log(num);
+// }
+
+// let number = 2;
+// for (let i = 1; i <= 10; i++) {
+//   let result = number * i;
+//   console.log(`${number}X${i}=${result}`);
+// }
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
---
+++ function.js
@@ -0,0 +1,52 @@
+// apple()
+// function abc() {
+//   console.log("This is a function")
+// }
+// abc()
+
+// function oppo() {
+//   console.log("This is a oppo")
+// }
+// oppo()
+// function samsung() {
+//   console.log("This is a samsung")
+// }
+// samsung()
+// function apple() {
+//   console.log("This is a apple")
+// }
+
+  // var a = 10
+  // var b = 20
+  // function add() {
+  //   console.log(a+b)
+  // }
+  // add()
+
+    // var fActor= "Kamal";
+    // var fPlayer="Dhoni";
+    // var fMovie="Fight Club"
+    // function favourite() {
+    //   console.log("Favourite Actor:" + fActor)
+    //   console.log("Favourite Player:" + fPlayer)
+    //   console.log("Favourite Movie:" + fMovie)
+    // }  
+    // favourite()
+  
+//parameter
+    // function multiply(a,b) {
+    //  console.log(a*b)
+    // }
+    // multiply(5,10)
+
+// function area(l,b)
+//    { 
+//     console.log("Area is:"+ l*b)
+//    }
+//    area(10,6)
+
+function add(a,b) {
+   return a+b
+  }
+var result= add(10,5);
+console.log(result)
